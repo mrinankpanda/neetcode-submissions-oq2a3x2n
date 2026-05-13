b2c972bf0b51eac375e91d62e55bf62023c9269d
Mrinank Panda <mrinank.panda@gmail.com>
2026-05-13 15:31:53 -0500

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return new ArrayList<>(); 
+        List<String> res = new ArrayList<>(); 
+        res.add(""); 
+        String[] digitToChar = {"", "", "abc", "def", "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"}; 
+        for (char digit : digits.toCharArray()) {
+            List<String> tmp = new ArrayList<>(); 
+            for (String curStr : res) {
+                for (char c : digitToChar[digit - '0'].toCharArray()) {
+                    tmp.add(curStr + c);
+                }
+            }
+            res = tmp; 
+        }
+        return res;
+    }
+}
